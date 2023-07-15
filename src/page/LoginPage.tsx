0b820190cf2738299e9edeb04292ebeb2324d210
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2023-07-16 03:05:02 +0600

    [Done]login successFull

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import '../Style/Athentication.css';
+import { useAppDispatch } from '../redux/hooks';
+import { loginUser } from '../redux/Fetures/user/userSlice';
 const LoginPage = () => {
   interface SignupFormInputs {
     email: string;
@@ -12,7 +15,9 @@ const LoginPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<SignupFormInputs>();
+  const dispatch = useAppDispatch();
   const HandleonSubmit = (data: SignupFormInputs) => {
+    dispatch(loginUser({ email: data.email, password: data.password }));
     console.log(data.email, data.password);
   };
 
--- src/redux/Fetures/user/userSlice.tsx
+++ src/redux/Fetures/user/userSlice.tsx
@@ -33,7 +33,13 @@ export const createUser = createAsyncThunk(
     return data.user.email;
   },
 );
-
+export const loginUser = createAsyncThunk(
+  'user/login',
+  async ({ email, password }: ICreadinTial) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  },
+);
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -57,6 +63,21 @@ const userSlice = createSlice({
         state.isLoading = false;
       })
       .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message!;
+      })
+      .addCase(loginUser.pending, state => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.user.email = null;
         state.isLoading = false;
         state.isError = true;
